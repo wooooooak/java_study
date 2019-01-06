a07757bb80b56ac576c40fa934f1f64620d407d1
LEE YONGJUN <wooooooak@gmail.com>
2019-01-06 20:37:34 +0900

    Exception 코드 정렬

--- CoreJava/src/Exception/MyException.java
+++ CoreJava/src/Exception/MyException.java
@@ -1,10 +1,10 @@
 package Exception;
 
-public class MyException extends  Exception {
+public class MyException extends Exception {
     private final String msg = "0~100 사이의 값만 입력 가능";
 
     @Override
     public String getLocalizedMessage() {
-        return  msg;
+        return msg;
     }
 }
