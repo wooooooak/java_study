3941fcc37ecb004aa537e207f0e3d56ae6b68a9a
wooooooak <wooooooak@gmail.com>
2019-02-11 20:11:16 +0900

    Singleton pattern study

---
+++ design_pattern/src/Singleton/Main.java
@@ -0,0 +1,17 @@
+package Singleton;
+
+public class Main {
+
+  public static void main(String[] args) {
+    SystemSpeaker speaker1 = SystemSpeaker.getInstance();
+    SystemSpeaker speaker2 = SystemSpeaker.getInstance();
+
+    System.out.println((speaker1.getVolume()));
+    System.out.println(speaker2.getVolume());
+
+    speaker1.setVolume(11);
+    System.out.println((speaker1.getVolume()));
+    System.out.println(speaker2.getVolume());
+  }
+
+}
---
+++ design_pattern/src/Singleton/SystemSpeaker.java
@@ -0,0 +1,26 @@
+package Singleton;
+
+public class SystemSpeaker {
+  static private SystemSpeaker instance;
+
+  private int volume;
+
+  private SystemSpeaker() {
+    volume = 5;
+  }
+
+  public static SystemSpeaker getInstance() {
+    if (instance == null) {
+      instance = new SystemSpeaker();
+    }
+    return instance;
+  }
+
+  public int getVolume() {
+    return volume;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+}
